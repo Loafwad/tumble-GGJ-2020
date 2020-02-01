27ed19a17c174d6cf1d81da72cbbd7efd1435b72
krzycho <krzycho_s@tlen.pl>
2020-02-01 01:36:20 +0100

    [PROG][SCENE][PREF] Core gameplay basics: - ActiveObjects GroundObjects, - win/fail conditions, - very basic runnig/resseting by pressing Space key - basic test scene

---
+++ Assets/Scripts/ActiveElement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ActiveElement : MonoBehaviour
+{
+	protected Vector2 originalPosition;
+	protected Quaternion originalRotation;
+
+	private Rigidbody2D _rigidbody2DCached;
+
+	public Rigidbody2D rigidbody2DCached
+	{
+		get
+		{
+			return _rigidbody2DCached;
+		}
+	}
+	// Start is called before the first frame update
+	void Start()
+	{
+		originalPosition = gameObject.transform.position;
+		originalRotation = gameObject.transform.rotation;
+		_rigidbody2DCached = GetComponent<Rigidbody2D>();
+		LevelController.instance.RegisterActiveElement(this);
+	}	public void Reset()
+	{
+		gameObject.transform.position = originalPosition;
+		gameObject.transform.rotation = originalRotation;
+		_rigidbody2DCached.velocity = Vector2.zero;
+		rigidbody2DCached.angularVelocity = 0f;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.gameObject.GetComponent<GroundElement>())
+		{
+			LevelController.instance.FailLevel();
+		}
+	}
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+	public static bool dontDestroyOnLoad = false;
+
+	// Check to see if we're about to be destroyed.
+	private static bool shuttingDown = false;
+	private static object lockObject = new object();
+	private static T _instance;
+
+	public static T instance
+	{
+		get
+		{
+			if (shuttingDown)
+			{
+				Debug.LogWarning("[Singleton] Instance '" + typeof(T) +
+					"' already destroyed. Returning null.");
+				return null;
+			}
+
+			lock (lockObject)
+			{
+				if (_instance == null)
+				{
+					// Search for existing instance.
+					_instance = (T)FindObjectOfType(typeof(T));
+
+					// Create new instance if one doesn't already exist.
+					if (_instance == null)
+					{
+						// Need to create a new GameObject to attach the singleton to.
+						var singletonObject = new GameObject();
+						_instance = singletonObject.AddComponent<T>();
+						singletonObject.name = typeof(T).ToString() + " (Singleton)";
+
+						if (dontDestroyOnLoad)
+						{
+							DontDestroyOnLoad(singletonObject);
+						}
+					}
+				}
+
+				return _instance;
+			}
+		}
+	}
+
+
+	private void OnApplicationQuit()
+	{
+		shuttingDown = true;
+	}
+
+
+	private void OnDestroy()
+	{
+		shuttingDown = true;
+	}
+}
